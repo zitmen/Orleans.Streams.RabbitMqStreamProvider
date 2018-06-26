c18ee7e00f470e81e6da464fa32ea78564879974
Martin Ovesny <zitmen@gmail.com>
2018-06-26 08:15:09 +0200

    better naming: RabbitMqStreamProviderIntegration --> RabbitMqStreamIntegrationProvider

---
+++ Orleans.Streams.RabbitMqStreamProvider/RabbitMqStreamIntegrationProvider.cs
@@ -0,0 +1,9 @@
+﻿using Orleans.Providers.Streams.Common;
+using Orleans.Streams.BatchContainer;
+
+namespace Orleans.Streams
+{
+    public class RabbitMqStreamIntegrationProvider<TSerializer> : PersistentStreamProvider<RabbitMqAdapterFactory<TSerializer>> where TSerializer : IBatchContainerSerializer, new()
+    {
+    }
+}
